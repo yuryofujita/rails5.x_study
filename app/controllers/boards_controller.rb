688bcd45040fe2201ffaf02db5098b08df2e966e
yuryofujita <dasman74@hotmail.com>
2020-07-09 07:17:08 +0900

    編集画面等作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,35 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
   end
 
   def new
     @board = Board.new
+    #binding.pry
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    p(@board)
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    board = Board.find(params[:id])
+    board.update(board_params)
+    redirect_to board
+  end
+
+  def create
+    Board.create(board_params)
+    binding.pry
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # post 'boards', to: 'boards#create'
+  # get 'boards/:id', to: 'boards#show'
+  #resources :boards
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
+
 end
